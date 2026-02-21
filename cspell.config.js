5966d49426282818656fc613d600af0a9c4ed59c
cl10k <1145346+cl10k@users.noreply.github.com>
2026-02-21 18:00:02 +0100

    bracket spacing fixed

--- cspell.config.js
+++ cspell.config.js
@@ -2,7 +2,7 @@
 
 /**
  * @see https://cspell.org/docs/Configuration
- * @type { import("@cspell/cspell-types").CSpellUserSettings }
+ * @type {import("@cspell/cspell-types").CSpellUserSettings}
  */
 export default {
   version: "0.2",
