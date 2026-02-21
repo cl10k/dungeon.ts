c824b5492eec7d3062315205ecb221f3b3d8422a
cl10k <1145346+cl10k@users.noreply.github.com>
2026-02-21 17:18:16 +0100

    cspell added

---
+++ cspell.config.js
@@ -0,0 +1,9 @@
+//? https://cspell.org/docs/Configuration
+
+/** @type {import("@cspell/cspell-types").CSpellUserSettings} */
+module.exports = {
+  version: "0.2",
+  language: "en",
+  ignorePaths: ["node_modules/**"],
+  words: [],
+};
