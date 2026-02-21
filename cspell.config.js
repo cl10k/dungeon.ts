aac4abae42ddcd697fa16832046f552608705168
cl10k <1145346+cl10k@users.noreply.github.com>
2026-02-21 17:35:45 +0100

    cspell config types added

--- cspell.config.js
+++ cspell.config.js
@@ -1,7 +1,8 @@
-//? https://cspell.org/docs/Configuration
-
-/** @type {import("@cspell/cspell-types").CSpellUserSettings} */
-module.exports = {
+/**
+ * @see https://cspell.org/docs/Configuration
+ * @type { import("@cspell/cspell-types").CSpellUserSettings }
+ */
+export default {
   version: "0.2",
   language: "en",
   ignorePaths: ["node_modules/**"],
